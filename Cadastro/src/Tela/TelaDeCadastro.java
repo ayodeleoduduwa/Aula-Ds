e0bc6795de3d5b2e38c29fd93e0b7181f882f2ae
ayodeleoduduwa <ayodeleoduduwa52@gmail.com>
2019-04-12 23:38:24 -0300

    Adicionado butoes

--- Cadastro/src/Tela/TelaDeCadastro.java
+++ Cadastro/src/Tela/TelaDeCadastro.java
@@ -13,48 +13,45 @@ import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
 public class TelaDeCadastro extends JFrame{
-	JTextField txtnome = new JTextField();
-	JTextField txtendereco = new JTextField();
-	JTextField txtcep= new JTextField();
-	JTextField txtcel = new JTextField();
-	JTextField txtCPF = new JTextField();
-	JTextField txtTel = new JTextField();
+	
+	
+	
+	JTextField txtNome = new JTextField();
+	JTextField txtend = new JTextField();
+	JTextField txtci = new JTextField();
+	JTextField txtbai = new JTextField();
+	JTextField txttel = new JTextField();
 	JTextField txtRG = new JTextField();
-    JTextField txtcidade = new JTextField();
-	JTextField txtbairro = new JTextField();
+	JRadioButton[] rdbSEX = new JRadioButton[2];
+	ButtonGroup grupo = new ButtonGroup();{
+	rdbSEX[0] = new JRadioButton("Feminino");
+	rdbSEX[1] = new JRadioButton("Masculino");}
+	JTextField txtCEP = new JTextField();
+	JComboBox cmbUF = new JComboBox();
+	JTextField txtCEL = new JTextField();
+	JTextField txtCPF = new JTextField();
 	
 	
-
-	JLabel lblcRG = new JLabel("RG:");
-	JLabel lblcSex = new JLabel("Sexo:");
-	JLabel lblcCPF = new JLabel("CPF:");
-	JLabel lblcbairro = new JLabel("Bairro:");
-    JLabel lblcnome = new JLabel("Nome:");
-	JLabel lblcendereco = new JLabel("Endereço:");
-	JLabel lblccep = new JLabel("CEP:");
-	JLabel lblcUF = new JLabel("Estado:");
-	JLabel lblccel = new JLabel("Celular:");
-	JLabel lblccidade = new JLabel("Cidade:");
-	JLabel lblctel = new JLabel("Telefone:");
-	JLabel lblsexo = new JLabel ("Sexo:");
+	JLabel lblnome = new JLabel("NOME:");
+	JLabel lblend = new JLabel("ENDEREÇO:");
+	JLabel lblci = new JLabel("CIDADE:");
+	JLabel lblba = new JLabel("BAIRRO:");
+	JLabel lbltel = new JLabel("TELEFONE:");
+	JLabel lblRg = new JLabel("RG:");
+	JLabel lblSEX = new JLabel("SEXO:");
+	JLabel lblCEP = new JLabel("CEP:");
+	JLabel lblEST = new JLabel("ESTADO:");
+	JLabel lblCEL = new JLabel("CELULAR:");
+	JLabel lblCPF = new JLabel("CPF:");
 	
 	JButton btnSALV = new JButton("Salvar");
 	JButton btnIMPRI = new JButton("Imprimir");
 	
-	JRadioButton[] rdbsexo = new JRadioButton[2]; 
-	ButtonGroup grupo = new  ButtonGroup();{
-	rdbsexo[0] = new JRadioButton("Masculino");
-	rdbsexo[1] = new JRadioButton("Feminino");}
- 
-	JComboBox CMUF = new JComboBox();
-	
-	MaskFormatter tel = null; 
-	MaskFormatter cel = null;
-	MaskFormatter cpf = null;
-	MaskFormatter cep = null;
-	MaskFormatter rg = null;
-	
-
+	MaskFormatter formTEL = null; 
+	MaskFormatter formCEL = null;
+	MaskFormatter formCPF = null;
+	MaskFormatter formCEP = null;
+	MaskFormatter formRG = null;
 	
 	
 	public TelaDeCadastro() {
@@ -63,125 +60,129 @@ public class TelaDeCadastro extends JFrame{
 		Container paine = this.getContentPane();
 		paine.setLayout(null);
 		
-
+		txtNome.setBounds(90, 20, 200, 30);
+		paine.add(txtNome);
+		lblnome.setBounds(10, 20, 200, 30);
+		paine.add(lblnome);
 		
-
-        txtRG.setBounds(80, 60, 200, 30);
-		paine.add(txtRG);
-		lblcRG.setBounds(10, 60, 170, 30);
-		paine.add(lblcRG);
+		txtend.setBounds(90, 60, 200, 30);
+		paine.add(txtend);
+		lblend.setBounds(10, 60, 200, 30);
+		paine.add(lblend);
 		
-		paine.add(lblccep);
-		lblccep.setBounds(290,60, 170, 30);
-		paine.add(txtcep);
-		txtcep.setBounds(320,60, 140,30);
+		txtci.setBounds(90, 100, 200, 30);
+		paine.add(txtci);
+		lblci.setBounds(10, 100, 200, 30);
+		paine.add(lblci);
 		
-		txtendereco.setBounds(80, 220, 200, 30);
-		paine.add(txtendereco);
-		lblcendereco.setBounds(10, 220, 122, 30);
-		paine.add(lblcendereco);
-		
-		
-		
-		txtnome.setBounds(80, 20, 200, 30);
-		paine.add(txtnome);
-		lblcnome.setBounds(10, 20, 150, 30);
-		paine.add(lblcnome);
-
-		txtcidade.setBounds(80, 100, 200, 30);
-		paine.add(txtcidade);
-		lblccidade.setBounds(10, 100, 140, 30);
-		paine.add(lblccidade);
-		
-		txtbairro.setBounds(80, 140, 200, 30);
-		paine.add(txtbairro);
-		lblcbairro.setBounds(10, 140, 140, 30);
-		paine.add(lblcbairro);
-		
-		paine.add(txtcel);
-		txtcel.setBounds(335, 140, 125, 30);	
-		paine.add(lblccel);
-		lblccel.setBounds(290, 140, 90, 30);
+		txtbai.setBounds(90, 140, 200, 30);
+		paine.add(txtbai);
+		lblba.setBounds(10, 140, 200, 30);
+		paine.add(lblba);
 		
 		
 		try {
-			tel = new MaskFormatter("(##)####-####"); 
-			txtTel= new JFormattedTextField(tel);
-		} catch (Exception ext) { 
+			formTEL = new MaskFormatter("(##)####-####");
+			txttel = new JFormattedTextField(formTEL);
+		} catch (Exception ext) {
 			ext.printStackTrace();
+		}
+		txttel.setBounds(90, 180, 200, 30);
+		paine.add(txttel);
 		
-		txtTel.setBounds(80, 180, 200, 30);
-		paine.add(txtTel);
-		lblctel.setBounds(10, 180, 125, 30);
-		paine.add(lblctel);
+		lbltel.setBounds(10, 180, 200, 30);
+		paine.add(lbltel);
 		
-        paine.add(lblsexo);
-        lblsexo.setBounds(290, 15, 125, 30);				
-		grupo.add(rdbsexo[0]); 
-		grupo.add(rdbsexo[1]);
-		paine.add(rdbsexo[0]); 
-		rdbsexo[0].setBounds(340, 15, 90, 30);
-		paine.add(rdbsexo[1]);
-		rdbsexo[1].setBounds(430, 15, 90, 30);
-	
+		try {
+			formRG = new MaskFormatter("##.###.###-A");
+			txtRG = new JFormattedTextField(formRG);
+		} catch (Exception exr) {
+			exr.printStackTrace();
+		}
+		txtRG.setBounds(90, 220, 200, 30);
+		paine.add(txtRG);
+		lblRg.setBounds(10, 220, 200, 30);
+		paine.add(lblRg);
 		
-		paine.add(lblcCPF);
-		lblcCPF.setBounds(290, 180, 125, 30);
+		grupo.add(rdbSEX[0]); 
+		grupo.add(rdbSEX[1]);
+		paine.add(rdbSEX[0]); 
+		rdbSEX[0].setBounds(400, 20, 85, 30);
+		paine.add(rdbSEX[1]);
+		rdbSEX[1].setBounds(500, 20, 90, 30);
+		lblSEX.setBounds(320, 20, 200, 30);
+		paine.add(lblSEX);
+		
+		try { 
+			formCEP = new MaskFormatter("###-#####");
+			txtCEP = new JFormattedTextField(formCEP);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		txtCEP.setBounds(400, 60, 185, 30);
+		paine.add(txtCEP);
+		lblCEP.setBounds(320, 60, 200, 30);
+		paine.add(lblCEP);
+		paine.add(cmbUF);
+		cmbUF.setBounds(400, 100, 185, 30);
+		cmbUF.addItem("AC");		cmbUF.addItem("AL");
+		cmbUF.addItem("AP");		cmbUF.addItem("AM");
+		cmbUF.addItem("BA");		cmbUF.addItem("CE");
+		cmbUF.addItem("DF");		cmbUF.addItem("ES");
+		cmbUF.addItem("GO");		cmbUF.addItem("MA");
+		cmbUF.addItem("MT");		cmbUF.addItem("MS");
+		cmbUF.addItem("MG");		cmbUF.addItem("PA");
+		cmbUF.addItem("PB");		cmbUF.addItem("PR");
+		cmbUF.addItem("PE");		cmbUF.addItem("PI");
+		cmbUF.addItem("RJ");		cmbUF.addItem("RN");
+		cmbUF.addItem("RS");		cmbUF.addItem("RO");
+		cmbUF.addItem("RR");		cmbUF.addItem("SC");
+		cmbUF.addItem("SP");		cmbUF.addItem("SE");
+		cmbUF.addItem("TO");	
+		
+		lblEST.setBounds(320, 100, 200, 30);
+		paine.add(lblEST);
+		try {
+			formCEL = new MaskFormatter("(##)#####-####");
+			txtCEL = new JFormattedTextField(formCEL);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		txtCEL.setBounds(400, 140, 185, 30);
+		paine.add(txtCEL);
+		lblCEL.setBounds(320, 140, 200, 30);
+		paine.add(lblCEL);
+		
+		
+		try {
+			formCPF = new MaskFormatter("###.###.###-##");
+			txtCPF = new JFormattedTextField(formCPF);
+		} catch (Exception exf) {
+			exf.printStackTrace();
+		}
+		txtCPF.setBounds(400, 180, 185, 30);
 		paine.add(txtCPF);
-        txtCPF.setBounds(320, 180, 140, 30);
-		
-		paine.add(lblcUF);
-		lblcUF.setBounds(290, 100, 90, 30);
-		
-		paine.add(CMUF);
-		CMUF.setBounds(340, 100, 90, 30);
-		CMUF.addItem("AC");
-		CMUF.addItem("AP");
-		CMUF.addItem("BA");
-		CMUF.addItem("DF");
-		CMUF.addItem("GO");
-		CMUF.addItem("MT");
-		CMUF.addItem("MG");
-		CMUF.addItem("PB");
-		CMUF.addItem("PE");
-		CMUF.addItem("RJ");
-		CMUF.addItem("RS");
-		CMUF.addItem("SP");
-		CMUF.addItem("TO");
-		CMUF.addItem("AL");
-		CMUF.addItem("AM");
-		CMUF.addItem("CE");
-		CMUF.addItem("ES");
-		CMUF.addItem("MA");
-		CMUF.addItem("MS");
-		CMUF.addItem("PA");
-		CMUF.addItem("PR");
-		CMUF.addItem("PI");
-		CMUF.addItem("RN");
-		CMUF.addItem("RO");
-		CMUF.addItem("SC");
-		CMUF.addItem("SE");
+		lblCPF.setBounds(320, 180, 200, 30);
+		paine.add(lblCPF);
 		
 		paine.add(btnSALV);
-		btnSALV.setBounds(160, 300, 140, 30);
+		btnSALV.setBounds(450, 250, 140, 30);
 		
 		paine.add(btnIMPRI);
-		btnIMPRI.setBounds(10, 300, 140, 30);
+		btnIMPRI.setBounds(300, 250, 140, 30);
 		
-	
-		
-				
-		
-		
-		paine.add(btnIMPRI);
-		
+			
 		this.setVisible(true);
-		this.setSize(500, 500);
+		this.setSize(615, 350);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		
-	}}
-	public static void main(String args[]) {
-		 TelaDeCadastro tela = new TelaDeCadastro();
 	}
-}
+		
+	
+	public static void main(String args[]) {
+		TelaDeCadastro tela = new TelaDeCadastro();
+	}
+	}
+	
